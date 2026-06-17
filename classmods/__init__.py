d936b9b719b84bea0f1585f2ef95dfb1062bb699
Mohammad Heydari <m_heydari@razi.cc>
2026-06-17 15:43:01 +0330

    updated_mainfest_to_match_org_rules

--- classmods/__init__.py
+++ classmods/__init__.py
@@ -1,12 +1,26 @@
-from .__version__ import get_version
 from ._decorators import logwrap, suppress_errors
 from ._descriptors import ConstantAttrib, RemoteAttrib
 from ._env_mod import ENVMod
 from ._method_monitor import MethodMonitor
 from ._super_with import SuperWith
 
+__version__ = '1.2.2'
+__description__ = "Simple mods for python classes."
+__authors__ = [
+    {"name": "devixa-dev", "email": "hmohammad2520@gmail.com"},
+]
+__keywords__ = [
+    "python",
+    "class",
+    "mods",
+    "modification",
+    "descriptors",
+    "methods",
+    "methodmod",
+    "methodobserve",
+]
+
 __all__ = [
-    'get_version',
     'ConstantAttrib',
     'RemoteAttrib',
     'ENVMod',
--- classmods/__version__.py
+++
@@ -1,8 +0,0 @@
-__version__ = '1.2.2'
-
-def get_version() -> str:
-    return __version__
-
-__all__ = [
-    '__all__',
-]
