59be7cfc0afba93224fda0c19f3a1ffb9dd0e64b
Mohammad Heydari <HMohammad2520@Gmail.com>
2026-07-06 15:38:37 +0330

    Fixed Build Problems

--- classmods/__init__.py
+++ classmods/__init__.py
@@ -8,20 +8,6 @@ from ._super_with import SuperWith
 from ._supress_errors import suppress_errors
 
 __version__ = '1.3.0'
-__description__ = "Simple mods for python classes."
-__authors__ = [
-    {"name": "devixa-dev", "email": "hmohammad2520@gmail.com"},
-]
-__keywords__ = [
-    "python",
-    "class",
-    "mods",
-    "modification",
-    "descriptors",
-    "methods",
-    "methodmod",
-    "methodobserve",
-]
 
 __all__ = [
     'classproperty',
